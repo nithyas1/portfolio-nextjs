abbb61b4199f84279cbc1f23f8fe9218f43e1300
Nithya Sasidharan <nithyas.mec@gmail.com>
2024-05-12 18:27:22 +0530

    fixed compilation error during deployment

--- app/mrbean/page.tsx
+++ app/mrbean/page.tsx
@@ -1,115 +1,128 @@
-import React from 'react';
+import React from "react";
 
-const Pattern = async() => {
-
+const Pattern = async () => {
   return (
     <div id="body">
-      <div  className="max-w-[75%] mx-auto rounded-2xl border p-12 shadow-lg shadow-indigo-300/40 select-none space-y-8">
-        <h1 className="text-3xl font-extrabold text-center">Mr. Bean&apos;s Teddy Keychain Pattern</h1>
-        <pre className='whitespace-pre-wrap text-justify'>
-        To crochet this keychain you will need:{'\n'}
-        {'\n'}
-        Any worsted weight yarn in the necessary colors (brown, beige and black).{'\n'}
-        2 mm crochet hook (or according to the specifications of the yarn){'\n'}
-        Embroidery thread in white color{'\n'}
-        Fiberfill{'\n'}
-        Scissors and sewing needle{'\n'}
-        {'\n'}
-        The finished teddy bear is about 10 cm (4″) long. Please note that the size of your work depends on yarn weight and hook size.{'\n'}
-        {'\n'}
-        Skill level: Beginner{'\n'}
-        {'\n'}
-        Abbreviations:{'\n'}
-        MR = magic ring{'\n'}
-        ch = chain{'\n'}
-        st = stitch{'\n'}
-        sc = single crochet{'\n'}
-        inc = increase (2 st in one st){'\n'}
-        dec = decrease (2 st together){'\n'}
-        sl st = slip stitch{'\n'}
-        (..) = repeat the instructions in brackets the given number of times{'\n'}
-        [..] = total number of stitches{'\n'}
-        {'\n'}
-        Crochet in continuous rounds, unless otherwise specified. Mark the beginning of each round with a stitch marker or a yarn scrap.{'\n'}
-        {'\n'}
-        Legs:{'\n'}
-        {'\n'}
-        Make 2.{'\n'}
-        With Beige yarn.{'\n'}
-        R1. MR. 6 sc. [6]{'\n'}
-        R2. inc*6. [12]{'\n'}
-        R3. 12 sc. [12]{'\n'}
-        Change to Brown yarn.{'\n'}
-        R4 - R15. 12 sc [18]{'\n'}
-        {'\n'}
-        Cut the yarn and fasten off on the first leg. For the second leg, continue crocheting to attach the legs together and continue building the body.{'\n'}
-        {'\n'}
-        Body:{'\n'}
-        {'\n'}
-        R1. Chain 4 on second leg. Join with sc to first leg. 11 sc on first leg. 4 sc into the connecting chain. 12 sc in second leg. [32]{'\n'}
-        R2 - R3. 32 sc.{'\n'}
-        R4. 9 sc. dec*2. 12 sc. dec*2. 3 sc. [28]{'\n'}
-        R5. 28 sc. [28]{'\n'}
-        R6. 8 sc. dec*2. 10 sc. dec*2. 2 sc. [24]{'\n'}
-        R7. 24 sc. [24]{'\n'}
-        R8. 7 sc. dec*2. 8 sc. dec*2. 1 sc. [20]{'\n'}
-        R9. 20 sc. [20]{'\n'}
-        R10. 6 sc. dec*2. 6 sc. dec*2. [16]{'\n'}
-        R11. 16 sc. [16]{'\n'}
-        R12. dec. 4 sc. dec*2. 4 sc. dec. [16]{'\n'}
-        R13. 12 sc. [12]{'\n'}
-        Cut the yarn and fasten off.{'\n'}
-        {'\n'}
-        Head:{'\n'}
-        {'\n'}
-        R1. chain 6. sc on the second st from the hook. 3 sc. 3 sc in the last chain. 3 sc on the other side of the chain. inc on the last chain. [12]{'\n'}
-        R2. inc in first st. 4 sc. inc in the next 2 sts. 4 sc. inc in the last st. [16]{'\n'}
-        R3. 2 inc. 4 sc. 3 inc. 6 sc. inc. [22]{'\n'}
-        R5 - R7. 28 sc. [22]{'\n'}
-        R8. (dec, 9 sc)*2 [20]{'\n'}
-        R9. (dec, 8 sc)*2 [18]{'\n'}
-        R10. (dec, 7 sc)*2 [16]{'\n'}
-        R11. (dec, 6 sc)*2 [14]{'\n'}
-        R12. (dec, 5 sc)*2 [12]{'\n'}
-        Cut the yarn, leave a long end for sewing.{'\n'}
-        {'\n'}
-        Hands:{'\n'}
-        {'\n'}
-        Make 2.{'\n'}
-        With Beige yarn.{'\n'}
-        R1. MR. 6 sc [6]{'\n'}
-        R2. inc*6 [12]{'\n'}
-        R3 - R4. 12 sc. [12]{'\n'}
-        Join Brown yarn.{'\n'}
-        R5 - R 16. 12 sc.{'\n'}
-        R17. (dec, 2 sc)*3  [9]{'\n'}
-        Cut the yarn, leave a long end for sewing.{'\n'}
-        {'\n'}
-        Ears:{'\n'}
-        {'\n'}
-        Make 2.{'\n'}
-        With Brown yarn.{'\n'}
-        R1. MR. 3 sc. [3]{'\n'}
-        R2. inc*3 [6]{'\n'}
-        R3 6 sc [6]{'\n'}
-        R4. (inc, sc)*3 [9]{'\n'}
-        Join both sides together and stich to close the opening. Leave a long end for sewing.{'\n'}
-        {'\n'}
-        Nose:{'\n'}
-        {'\n'}
-        With Black yarn.{'\n'}
-        R1. chain 5. sc in the second chain from the hook. 2 sc. 2 sc in the last chain. 3 sc on the other side of the chain. [8]{'\n'}
-        R2. 8 sc. [8]{'\n'}
-        Cut the yarn, leave a long end for sewing.{'\n'}
-        {'\n'}
-        Assembly:{'\n'}
-        {'\n'}
-        Place the head on the body and sew them together.{'\n'}
-        Attach the hands to the sides of the body.{'\n'}
-        Position the ears on the head and sew them.{'\n'}
-        Position the nose in front of the face and sew it on. {'\n'}
-        Embroider a small white mark on the nose.{'\n'}
-        Use the black yarn to embroider the eyes.{'\n'}
+      <div className="max-w-[75%] mx-auto rounded-2xl border p-12 shadow-lg shadow-indigo-300/40 select-none space-y-8">
+        <h1 className="text-3xl font-extrabold text-center">
+          Mr. Bean&apos;s Teddy Keychain Pattern
+        </h1>
+        <pre className="whitespace-pre-wrap text-justify">
+          To crochet this keychain you will need:{"\n"}
+          {"\n"}
+          Any worsted weight yarn in the necessary colors (brown, beige and
+          black).{"\n"}2 mm crochet hook (or according to the specifications of
+          the yarn){"\n"}
+          Embroidery thread in white color{"\n"}
+          Fiberfill{"\n"}
+          Scissors and sewing needle{"\n"}
+          {"\n"}
+          The finished teddy bear is about 10 cm (4″) long. Please note that the
+          size of your work depends on yarn weight and hook size.{"\n"}
+          {"\n"}
+          Skill level: Beginner{"\n"}
+          {"\n"}
+          Abbreviations:{"\n"}
+          MR = magic ring{"\n"}
+          ch = chain{"\n"}
+          st = stitch{"\n"}
+          sc = single crochet{"\n"}
+          inc = increase (2 st in one st){"\n"}
+          dec = decrease (2 st together){"\n"}
+          sl st = slip stitch{"\n"}
+          (..) = repeat the instructions in brackets the given number of times
+          {"\n"}
+          [..] = total number of stitches{"\n"}
+          {"\n"}
+          Crochet in continuous rounds, unless otherwise specified. Mark the
+          beginning of each round with a stitch marker or a yarn scrap.{"\n"}
+          {"\n"}
+          Legs:{"\n"}
+          {"\n"}
+          Make 2.{"\n"}
+          With Beige yarn.{"\n"}
+          R1. MR. 6 sc. [6]{"\n"}
+          R2. inc*6. [12]{"\n"}
+          R3. 12 sc. [12]{"\n"}
+          Change to Brown yarn.{"\n"}
+          R4 - R15. 12 sc [18]{"\n"}
+          {"\n"}
+          Cut the yarn and fasten off on the first leg. For the second leg,
+          continue crocheting to attach the legs together and continue building
+          the body.{"\n"}
+          {"\n"}
+          Body:{"\n"}
+          {"\n"}
+          R1. Chain 4 on second leg. Join with sc to first leg. 11 sc on first
+          leg. 4 sc into the connecting chain. 12 sc in second leg. [32]{"\n"}
+          R2 - R3. 32 sc.{"\n"}
+          R4. 9 sc. dec*2. 12 sc. dec*2. 3 sc. [28]{"\n"}
+          R5. 28 sc. [28]{"\n"}
+          R6. 8 sc. dec*2. 10 sc. dec*2. 2 sc. [24]{"\n"}
+          R7. 24 sc. [24]{"\n"}
+          R8. 7 sc. dec*2. 8 sc. dec*2. 1 sc. [20]{"\n"}
+          R9. 20 sc. [20]{"\n"}
+          R10. 6 sc. dec*2. 6 sc. dec*2. [16]{"\n"}
+          R11. 16 sc. [16]{"\n"}
+          R12. dec. 4 sc. dec*2. 4 sc. dec. [16]{"\n"}
+          R13. 12 sc. [12]{"\n"}
+          Cut the yarn and fasten off.{"\n"}
+          {"\n"}
+          Head:{"\n"}
+          {"\n"}
+          R1. chain 6. sc on the second st from the hook. 3 sc. 3 sc in the last
+          chain. 3 sc on the other side of the chain. inc on the last chain.
+          [12]{"\n"}
+          R2. inc in first st. 4 sc. inc in the next 2 sts. 4 sc. inc in the
+          last st. [16]{"\n"}
+          R3. 2 inc. 4 sc. 3 inc. 6 sc. inc. [22]{"\n"}
+          R5 - R7. 28 sc. [22]{"\n"}
+          R8. (dec, 9 sc)*2 [20]{"\n"}
+          R9. (dec, 8 sc)*2 [18]{"\n"}
+          R10. (dec, 7 sc)*2 [16]{"\n"}
+          R11. (dec, 6 sc)*2 [14]{"\n"}
+          R12. (dec, 5 sc)*2 [12]{"\n"}
+          Cut the yarn, leave a long end for sewing.{"\n"}
+          {"\n"}
+          Hands:{"\n"}
+          {"\n"}
+          Make 2.{"\n"}
+          With Beige yarn.{"\n"}
+          R1. MR. 6 sc [6]{"\n"}
+          R2. inc*6 [12]{"\n"}
+          R3 - R4. 12 sc. [12]{"\n"}
+          Join Brown yarn.{"\n"}
+          R5 - R 16. 12 sc.{"\n"}
+          R17. (dec, 2 sc)*3 [9]{"\n"}
+          Cut the yarn, leave a long end for sewing.{"\n"}
+          {"\n"}
+          Ears:{"\n"}
+          {"\n"}
+          Make 2.{"\n"}
+          With Brown yarn.{"\n"}
+          R1. MR. 3 sc. [3]{"\n"}
+          R2. inc*3 [6]{"\n"}
+          R3 6 sc [6]{"\n"}
+          R4. (inc, sc)*3 [9]{"\n"}
+          Join both sides together and stich to close the opening. Leave a long
+          end for sewing.{"\n"}
+          {"\n"}
+          Nose:{"\n"}
+          {"\n"}
+          With Black yarn.{"\n"}
+          R1. chain 5. sc in the second chain from the hook. 2 sc. 2 sc in the
+          last chain. 3 sc on the other side of the chain. [8]{"\n"}
+          R2. 8 sc. [8]{"\n"}
+          Cut the yarn, leave a long end for sewing.{"\n"}
+          {"\n"}
+          Assembly:{"\n"}
+          {"\n"}
+          Place the head on the body and sew them together.{"\n"}
+          Attach the hands to the sides of the body.{"\n"}
+          Position the ears on the head and sew them.{"\n"}
+          Position the nose in front of the face and sew it on. {"\n"}
+          Embroider a small white mark on the nose.{"\n"}
+          Use the black yarn to embroider the eyes.{"\n"}
         </pre>
       </div>
     </div>
